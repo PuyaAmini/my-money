74ae0f1d4eb39c18701e3a7a5678470f3d646062
pamini <puyagit@gmail.com>
2024-05-27 13:11:07 +0330

    add Navbar [s14 e3] git-114

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import './App.css'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 // pages:
 import Home from './pages/home/Home';
 import Signup from './pages/signup/Signup';
 import LogIn from './pages/login/LogIn';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <Navbar/>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/signup' element={<Signup/>}/>
