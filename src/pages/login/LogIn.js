ceceb64bcfc9567c6abf9c96d88d8c9e28915294
pamini <puyagit@gmail.com>
2024-05-27 13:25:12 +0330

    login page form [s14 e4] git-115

--- src/pages/login/LogIn.js
+++ src/pages/login/LogIn.js
@@ -1,11 +1,37 @@
-import React from 'react'
-import styles from './Login.module.css'
+import { useState } from "react";
 
+import React from "react";
+import styles from "./Login.module.css";
 
 export default function LogIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSubmit = (e) =>{
+    e.preventDefault()
+    console.log(email , password)
+  }
   return (
-    <div>
-      <p>login</p>
-    </div>
-  )
+    <form className={styles["login-form"]} onSubmit={handleSubmit}>
+      <h2>Login</h2>
+      <label>
+        <span>email:</span>
+        <input
+          type="text"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+
+      <label>
+        <span>password</span>
+        <input
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+
+      <button className="btn">Login</button>
+    </form>
+  );
 }
