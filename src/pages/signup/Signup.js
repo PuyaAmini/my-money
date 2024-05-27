01bec7716e4fe380689fbb40735cef82829dd1b8
pamini <puyagit@gmail.com>
2024-05-27 22:28:21 +0330

    Creating a Signup Hook [s14 e8] git-119

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -13,26 +13,32 @@ export default function Signup() {
 
   return (
     <form className={styles["signup-form"]} onSubmit={handleSubmit}>
+      
       <h2>SignUp</h2>
+
       <label>
         <span>display name:</span>
         <input type="text" 
         onChange={(e) => setDisplayName(e.target.value)}
         value={displayName} />
       </label>
+
       <label>
         <span>Email:</span>
         <input type="email" 
         onChange={(e) => setEmail(e.target.value)}
         value={email} />
       </label>
+
       <label>
         <span>Password:</span>
         <input type="password" 
         onChange={(e) => setPassword(e.target.value)} 
         value={password}/>
       </label>
+
       <button className="btn">Signup</button>
+      
       <label></label>
     </form>
   );
